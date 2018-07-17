ae8fdc815257b743720dbd8ee70ef1d6dab534de
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-07-17 22:29:32 +0300

    refactor(App): get rid of App-intro

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">💩 ~ Fake News ~ 💩</h1>
         </header>
-        <p className="App-intro">
-          <Posts />
-        </p>
+        <Posts />
       </div>
     );
   }

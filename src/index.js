04006838be7e7a70e411c7c1db71ab40c9b1d76b
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-08-15 18:48:10 +0300

    feat(app): get rid of redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
-import rootReducer from "./reducers";
-import "./index.css";
-import App from "./App";
-import { root } from "./saga";
-import registerServiceWorker from "./registerServiceWorker";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
-sagaMiddleware.run(root);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,6 +11,6 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 registerServiceWorker();

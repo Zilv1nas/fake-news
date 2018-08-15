7a4baae3b59c383e4e46dee27aaca5da835969dd
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-08-15 22:22:20 +0300

    refactor(actions): remove unused import

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,5 +1,3 @@
-import { api } from "../services";
-
 export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
 export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
 export const ADD_POST_FAILURE = "ADD_POST_FAILURE";

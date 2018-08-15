d50b60fc7e978d98ece2c1165be1100d942c044f
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-08-15 22:38:00 +0300

    feat(app): add Posts store to the state Provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
+import { Posts } from './stores';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const stores = {
+  posts: new Posts(),
+};
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider {...stores}>
     <BrowserRouter>
       <App />
     </BrowserRouter>

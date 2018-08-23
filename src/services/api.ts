c5b06a863d7feee17f3eb6f602f26320dfe192e6
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-08-23 20:30:01 +0300

    refactor(api): avoid tslint errors for console.log

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,10 @@ export const requests = {
     };
     return fetch(url, config)
       .then(response => response.json())
-      .catch(error => console.error(`GET[${url}] failed =\n`, error));
+      .catch(error => {
+        // tslint:disable-next-line
+        console.error(`GET[${url}] failed =\n`, error);
+      });
   },
   post: (url: string, body = {}) => {
     const config = {
@@ -16,12 +19,15 @@ export const requests = {
     };
     return fetch(url, config)
       .then(response => response.json())
-      .catch(error => console.error(`POST[${url} failed =\n`, error));
+      .catch(error => {
+        // tslint:disable-next-line
+        console.error(`POST[${url} failed =\n`, error);
+      });
   }
 };
 
 export const Posts = {
-  all: () => requests.get("/posts?_sort=id&_order=desc"),
   add: ({ title, body }) => requests.post("/posts", { title, body }),
+  all: () => requests.get("/posts?_sort=id&_order=desc"),
   findOne: id => requests.get(`/posts/${id}`)
 };

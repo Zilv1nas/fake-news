ef4bd87e7d6c8f1968066f84f7f2f39cc8a756f3
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2018-08-23 22:09:36 +0300

    fix(routes): obey tslint/jsx-no-lambda

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import { Post, PostList } from './components/posts';
 
+const PageNotFound = () => (
+  <div>The page was not found.</div>
+);
+
 const routes = (
   <Switch>
     <Route path="/" exact={true} component={PostList} />
     <Route path="/posts/:id" exact={true} component={Post} />
-    <Route render={() => <div>The page was not found.</div>} />
+    <Route component={PageNotFound} />
   </Switch>
 );
 
